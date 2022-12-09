8c6725eb3765f3adbe9c60a51db59ef5fe85bd6a
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-12-09 15:23:16 +0700

    Rename States to users

--- src/states/users/reducer.js
+++ src/states/users/reducer.js
@@ -1,13 +1,13 @@
 import { ActionType } from './action';
 import { ActionType as ErrorActionType } from '../error/action';
 
-export default function usersReducer(state = [], action = {}) {
+export default function usersReducer(users = [], action = {}) {
   switch (action.type) {
     case ActionType.RECEIVE_USERS:
       return action.payload.users;
     case ErrorActionType.ERROR:
       return action.payload.error;
     default:
-      return state;
+      return users;
   }
 }
